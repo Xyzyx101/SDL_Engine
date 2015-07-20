6e604c0e7ca0fd455992159c327e23ce22791e56
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-07-20 09:30:05 -0400

    Fixed error handling and cleaned up the code a bit

--- SDL_Engine/SDL_Engine/Game.cpp
+++ SDL_Engine/SDL_Engine/Game.cpp
@@ -6,94 +6,97 @@
 #define SCREEN_HEIGHT 768
 
 Game::Game() :
-window_(0),
-renderer_(0),
-testImage_(0),
-texture_(0),
-surface_(0)
-{
-}
+window_( 0 ),
+renderer_( 0 ),
+testImage_( 0 ),
+texture_( 0 ),
+surface_( 0 ) {}
 
-Game::~Game()
-{
-}
+Game::~Game() {}
 
 int Game::InitSDL() {
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-		fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
-		return -1;
+	if( SDL_Init( SDL_INIT_EVERYTHING )<0 ) {
+		fprintf( stderr, "Unable to init SDL: %s\n", SDL_GetError() );
+		SDL_Quit();
+		return 1;
 	}
 
-	if ((window_ = SDL_CreateWindow(
-		"Bacon",
+	window_ = SDL_CreateWindow(
+		"SDL Test Window",
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
 		SCREEN_WIDTH,
 		SCREEN_HEIGHT,
-		SDL_WINDOW_FULLSCREEN_DESKTOP)
-		) == NULL) {
-		return -2;
+		SDL_WINDOW_FULLSCREEN_DESKTOP );
+	if( window_==nullptr ) {
+		fprintf( stderr, "Create window failed: %s\n", SDL_GetError() );
+		SDL_Quit();
+		return 2;
 	}
-	renderer_ = SDL_CreateRenderer(window_, -1, 0);
-
-	texture_ = SDL_CreateTexture(
-		renderer_,
-		SDL_PIXELFORMAT_ABGR8888,
-		SDL_TEXTUREACCESS_STREAMING,
-		SCREEN_WIDTH, SCREEN_HEIGHT);
+	renderer_ = SDL_CreateRenderer( window_, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC );
+	if( renderer_==nullptr ) {
+		fprintf( stderr, "Create Renderer fails: %s\n", SDL_GetError() );
+		SDL_DestroyWindow( window_ );
+		SDL_Quit();
+		return 3;
+	}
+	SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "linear" );  // make the scaled rendering look smoother.
+	SDL_RenderSetLogicalSize( renderer_, SCREEN_WIDTH, SCREEN_HEIGHT );
 	return 0;
 }
 
-int Game::InitTestImage()
-{
-	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");  // make the scaled rendering look smoother.
-	SDL_RenderSetLogicalSize(renderer_, SCREEN_WIDTH, SCREEN_HEIGHT);
-	surface_ = IMG_Load("party.png");
-	if (surface_ == NULL) {
-		printf("Image failed to load");
-		return -1;
+int Game::InitTestImage() {
+	surface_ = IMG_Load( "party.png" );
+	if( surface_==NULL ) {
+		fprintf( stderr, "Image failed to load: %s\n", IMG_GetError() );
+		SDL_DestroyWindow( window_ );
+		SDL_Quit();
+		return 4;
 	}
-	printf("surface: %p\n", surface_);
- 	printf("pixels: %p\n", surface_->pixels);
+	texture_ = SDL_CreateTextureFromSurface( renderer_, surface_ );
+	SDL_FreeSurface( surface_ );
+	if( texture_==nullptr ) {
+		fprintf( stderr, "Failed to create texture: %s\n", SDL_GetError() );
+		SDL_DestroyRenderer( renderer_ );
+		SDL_DestroyWindow( window_ );
+		SDL_Quit();
+		return 5;
+	}
 	return 0;
 }
 
-void Game::Run()
-{
-	int i = 0; while (++i < 500) {
+void Game::Run() {
+	int i = 0; while( ++i<5 ) {
 		Draw();
+		SDL_Delay( 1000 );
 	}
 }
 
-void Game::Draw()
-{
-	int err = SDL_UpdateTexture(texture_, NULL, surface_->pixels, surface_->pitch);
-	if (err != 0) {
-		fprintf(stdin, "Error in SDL_UpdateTexture");
-	}
-	SDL_RenderClear(renderer_);
+void Game::Draw() {
+	//draws 1/4 of the image on 1/4 of the screen
+
 	SDL_Rect srcRect = SDL_Rect();
 	srcRect.x = 0;
 	srcRect.y = 0;
-	srcRect.w = surface_->w / 2;
-	srcRect.h = surface_->h / 2;
+	srcRect.w = surface_->w/2;
+	srcRect.h = surface_->h/2;
 
 	SDL_Rect dstRect = SDL_Rect();
 	dstRect.x = 0;
-
-
 	dstRect.y = 0;
-	dstRect.w = SCREEN_WIDTH / 2;
-	dstRect.h = SCREEN_HEIGHT / 2;
+	dstRect.w = SCREEN_WIDTH/2;
+	dstRect.h = SCREEN_HEIGHT/2;
 
-	SDL_RenderCopy(renderer_, texture_, &srcRect, &dstRect);
-	if (err != 0) {
-		fprintf(stdin, "Error in SDL_RenderCopy");
-	}
-	SDL_RenderPresent(renderer_);
+	//clear renderer
+	SDL_RenderClear( renderer_ );
+
+	//draw texture
+	SDL_RenderCopy( renderer_, texture_, &srcRect, &dstRect );
+
+	//update screen
+	SDL_RenderPresent( renderer_ );
 }
 
-void Game::Clean()
-{
+void Game::Clean() {
 	SDL_Quit();
 }
--- SDL_Engine/SDL_Engine/Game.h
+++ SDL_Engine/SDL_Engine/Game.h
@@ -1,22 +1,21 @@
 #pragma once
 #include "SDL.h"
 
-class Game
-{
-	SDL_Window* window_;
-	SDL_Renderer* renderer_;
-	SDL_Window* testImage_;
-	SDL_Texture* texture_;
-	SDL_Surface* surface_;
-
+class Game {
 public:
 	Game();
 	~Game();
-
 	int InitSDL();
 	int InitTestImage();
 	void Run();
 	void Draw();
 	void Clean();
+
+private:
+	SDL_Window*		window_;
+	SDL_Renderer*	renderer_;
+	SDL_Window*		testImage_;
+	SDL_Texture*	texture_;
+	SDL_Surface*	surface_;
 };
 
