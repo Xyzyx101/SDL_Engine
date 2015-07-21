7e7503dc0bf2351aac5567c9f2929f3968fe8801
Andrew Perrault <andrew.perrault@yahoo.ca>
2015-07-21 07:20:26 -0400

    Directional movement events work

--- SDL_Engine/SDL_Engine/Game.cpp
+++ SDL_Engine/SDL_Engine/Game.cpp
@@ -3,17 +3,22 @@
 #include "SDL_image.h"
 #include "MathUtils.h"
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
+#define DEFAULT_SCREEN_WIDTH 1024
+#define DEFAULT_SCREEN_HEIGHT 768
 
-Game::Game() :
+#define CLEAR_COLOR_R 0
+#define CLEAR_COLOR_G 0
+#define CLEAR_COLOR_B 0
+
+Game::Game( void ) :
+running_( true ),
 window_( 0 ),
 renderer_( 0 ),
-testImage_( 0 ),
-texture_( 0 ),
-surface_( 0 ) {}
+screenWidth_( DEFAULT_SCREEN_WIDTH ),
+screenHeight_( DEFAULT_SCREEN_HEIGHT ),
+testImage_( 0 ) {}
 
-Game::~Game() {}
+Game::~Game( void ) {}
 
 int Game::InitSDL() {
 	if( SDL_Init( SDL_INIT_EVERYTHING )<0 ) {
@@ -24,11 +29,11 @@ int Game::InitSDL() {
 
 	window_ = SDL_CreateWindow(
 		"SDL Test Window",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH,
-		SCREEN_HEIGHT,
-		SDL_WINDOW_FULLSCREEN_DESKTOP );
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		screenWidth_,
+		screenHeight_,
+		SDL_WINDOW_SHOWN );
 	if( window_==nullptr ) {
 		fprintf( stderr, "Create window failed: %s\n", SDL_GetError() );
 		SDL_Quit();
@@ -42,21 +47,21 @@ int Game::InitSDL() {
 		return 3;
 	}
 	SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "linear" );  // make the scaled rendering look smoother.
-	SDL_RenderSetLogicalSize( renderer_, SCREEN_WIDTH, SCREEN_HEIGHT );
+	SDL_RenderSetLogicalSize( renderer_, screenWidth_, screenHeight_ );
 	return 0;
 }
 
 int Game::InitTestImage() {
-	surface_ = IMG_Load( "party.png" );
-	if( surface_==NULL ) {
+	SDL_Surface* surface = IMG_Load( "party.png" );
+	if( surface==NULL ) {
 		fprintf( stderr, "Image failed to load: %s\n", IMG_GetError() );
 		SDL_DestroyWindow( window_ );
 		SDL_Quit();
 		return 4;
 	}
-	texture_ = SDL_CreateTextureFromSurface( renderer_, surface_ );
-	SDL_FreeSurface( surface_ );
-	if( texture_==nullptr ) {
+	testImage_ = SDL_CreateTextureFromSurface( renderer_, surface );
+	SDL_FreeSurface( surface );
+	if( testImage_==nullptr ) {
 		fprintf( stderr, "Failed to create texture: %s\n", SDL_GetError() );
 		SDL_DestroyRenderer( renderer_ );
 		SDL_DestroyWindow( window_ );
@@ -67,35 +72,76 @@ int Game::InitTestImage() {
 }
 
 void Game::Run() {
-	int i = 0; while( ++i<5 ) {
+	SDL_Event newEvent;
+	while( running_ ) {
+		while( SDL_PollEvent( &newEvent ) ) {
+			handleEvent( newEvent );
+		}
+		pos_ += velocity_;
 		Draw();
-		SDL_Delay( 1000 );
 	}
 }
 
 void Game::Draw() {
-	//draws 1/4 of the image on 1/4 of the screen
-
-	SDL_Rect srcRect = SDL_Rect();
-	srcRect.x = 0;
-	srcRect.y = 0;
-	srcRect.w = surface_->w/2;
-	srcRect.h = surface_->h/2;
-
-	SDL_Rect dstRect = SDL_Rect();
-	dstRect.x = 0;
-	dstRect.y = 0;
-	dstRect.w = SCREEN_WIDTH/2;
-	dstRect.h = SCREEN_HEIGHT/2;
-
 	//clear renderer
+	SDL_SetRenderDrawColor( renderer_, CLEAR_COLOR_R, CLEAR_COLOR_G, CLEAR_COLOR_B, 255 );
 	SDL_RenderClear( renderer_ );
 
+	SDL_Rect dstRect;
+	dstRect.x = (Sint16)pos_.x;
+	dstRect.y = (Sint16)pos_.y;
+	dstRect.w = screenWidth_;
+	dstRect.h = screenHeight_;
+
 	//draw texture
-	SDL_RenderCopy( renderer_, texture_, &srcRect, &dstRect );
+	SDL_RenderCopy( renderer_, testImage_, NULL, &dstRect );
 
 	//update screen
 	SDL_RenderPresent( renderer_ );
+}
+
+void Game::handleEvent( const SDL_Event& e ) {
+	switch( e.type ) {
+	case SDL_QUIT:
+		running_ = false;
+		break;
+	case SDL_KEYDOWN:
+		onKeyDown( e.key.keysym.sym );
+		break;
+	case SDL_KEYUP:
+		onKeyUp( e.key.keysym.sym );
+		break;
+	}
+}
+
+void Game::onKeyDown( Uint32 key ) {
+	if( key==SDLK_UP ) {
+		velocity_.y -= 1.0f;
+	}
+	if( key==SDLK_DOWN ) {
+		velocity_.y += 1.0f;
+	}
+	if( key==SDLK_LEFT ) {
+		velocity_.x -= 1.0f;
+	}
+	if( key==SDLK_RIGHT ) {
+		velocity_.x += 1.0f;
+	}
+}
+
+void Game::onKeyUp( Uint32 key ) {
+	if( key==SDLK_UP ) {
+		velocity_.y = 0.0f;
+	}
+	if( key==SDLK_DOWN ) {
+		velocity_.y = 0.0f;
+	}
+	if( key==SDLK_LEFT ) {
+		velocity_.x = 0.0f;
+	}
+	if( key==SDLK_RIGHT ) {
+		velocity_.x = 0.0f;
+	}
 }
 
 void Game::Clean() {
--- SDL_Engine/SDL_Engine/Game.h
+++ SDL_Engine/SDL_Engine/Game.h
@@ -1,21 +1,31 @@
 #pragma once
 #include "SDL.h"
+#include "MathUtils.h"
 
 class Game {
 public:
-	Game();
-	~Game();
+	Game(void);
+	~Game(void);
 	int InitSDL();
 	int InitTestImage();
 	void Run();
-	void Draw();
 	void Clean();
+protected:
+	void clearBackBuffer();
+	void handleEvent( const SDL_Event& newEvent );
+	virtual void Draw();
+	virtual void onKeyDown( Uint32 key );
+	virtual void onKeyUp( Uint32 key );
+
+	Vec2	velocity_, pos_;
+	bool	running_;
+	Uint32	clearColor_;
+	Uint16	screenWidth_;
+	Uint16	screenHeight_;
 
 private:
 	SDL_Window*		window_;
 	SDL_Renderer*	renderer_;
-	SDL_Window*		testImage_;
-	SDL_Texture*	texture_;
-	SDL_Surface*	surface_;
+	SDL_Texture*	testImage_;
 };
 
